4d000c1bb406b6188fc29077412a246dcf4e4871
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-08-01 18:35:10 -0700

    finished server.js, waiting on twitter.com developer account approval

--- server.js
+++ server.js
@@ -11,3 +11,50 @@ const TWITTER_CONFIG = {
     consumerSecret: 'my secret from twitter', 
     callbackURL: 'http etc'
 }
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(express.json)
+app.use(passport.initialize())
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
+app.use(session({
+    secret: 'KeyboardKittens',
+    resave: true, 
+    saveUninitialized: true
+}))
+
+passport.serializeUser((user, cb) => cb(null, user))
+passport.deserializeUser((user, cb) => cb(null, obj))
+
+passport.use(new TwitterStrategy(
+    TWITTER_CONFIG,
+    (accessToken, refreshToken, profile, cb) => {
+        const user = {
+            name: profile.username, 
+            photo: profile.photos[0].value.replace(/_normal/, '')
+        }
+        cb(null, user)
+    })
+)
+
+const twitterAuth = passport.authenticate('twitter')
+
+const addSocketIdToSession = (req, res, next) => {
+    req.session.socketId = req.query.socketId
+    next()
+}
+app.get('/twitter', addSocketIdToSession, twitterAuth)
+
+app.get('twitter/callback', twitterAuth, (req, res) => {
+    io.in(req.session.socketId).emit('user', req.user)
+    res.end()
+})
+server.listen(8080, () => {
+    console.log('listening...')
+})
